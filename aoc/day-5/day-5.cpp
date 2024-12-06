dea4e2ee59f8fc294e8d6e55a2d4b799336c7ef0
James Collerton <jamescollerton@live.co.uk>
2024-12-06 19:49:54 +0000

    Before fixing

---
+++ aoc/day-5/day-5.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include "../io/io.h"
+
+using namespace std;
+
+// Create a map:
+//  Key - Number 
+//  Value - A set of all numbers that can only come after a number
+
+// From the end, move backwards
+
+struct ParsedInput {
+    map<int, set<int>> key_to_afters_map;
+    vector<vector<int>> page_orders;
+};
+
+void printMap(map<int, set<int>> key_to_afters_map) {
+    for(auto it = key_to_afters_map.cbegin(); it != key_to_afters_map.cend(); ++it) {
+        cout << to_string(it->first) << ": ";
+        for(auto in_it = it->second.cbegin(); in_it != it->second.cend(); ++in_it) {
+            cout << to_string(*in_it) << " ";
+        }
+        cout << endl;
+    }
+}
+
+void printPageOrders(vector<vector<int>> page_orders) {
+    for(int i = 0; i < page_orders.size(); i++) {
+        for(int j = 0; j < page_orders[i].size(); j++) {
+            cout << to_string(page_orders[i][j]) << " ";
+        }
+        cout << endl;
+    }
+}
+
+ParsedInput parseInput(vector<string> input) {
+
+    map<int, set<int>> key_to_afters_map;
+    int pos = 0;
+
+    while(input[pos].find('|') != std::string::npos) {
+        auto line = input[pos];
+
+        auto left = stoi(line.substr(0, line.find('|')));
+        auto right = stoi(line.substr(line.find('|') + 1));
+
+        if(key_to_afters_map.count(left) > 0) {
+            key_to_afters_map[left].insert(right);
+        } else {
+            key_to_afters_map[left] = set<int>{right};
+        }
+        pos++;
+    }
+
+    // Skip blank line
+    pos++;
+
+    vector<vector<int>> page_orders;
+
+    while(pos < input.size()) {
+        stringstream ss(input[pos]);
+        vector<int> num_vec;
+        string temp_s;
+
+        while(getline(ss, temp_s, ',')) {
+            num_vec.push_back(stoi(temp_s));
+        }
+
+        page_orders.emplace_back(num_vec);
+        pos++;   
+    }
+
+    return {
+        key_to_afters_map,
+        page_orders
+    };
+}
+
+bool isLineValid(map<int, set<int>> key_to_afters_map, vector<int> line) {
+
+    for(int i = line.size() - 1; i > 0; i--) {
+        set<int> banned_nums = key_to_afters_map[line[i]];
+        for(int j = 0; j < i; j++) {
+            if(banned_nums.count(line[j]) > 0) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void partOne(ParsedInput parsed_input) {
+
+    int total = 0;
+
+    for(auto line: parsed_input.page_orders) {
+        if(isLineValid(parsed_input.key_to_afters_map, line)) {
+            total += line[line.size() / 2];
+        }
+    }
+
+    cout << "Part one: " << total << endl;
+
+}
+
+void printLine(vector<int> line) {
+    cout << "Line: ";
+    for(auto i : line) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void printMap(map<int, int> map) {
+    for(auto it = map.cbegin(); it != map.cend(); ++it) {
+        cout << to_string(it->first) << ": " << to_string(it->second) << endl;
+    }
+}
+
+vector<int> orderLine(map<int, set<int>> key_to_afters_map, vector<int> line) {
+
+    vector<int> reordered_line = line;
+    map<int, int> num_to_position;
+
+    for(int i = 0; i < line.size(); i++) {
+        num_to_position[line[i]] = i;
+    }
+
+    int i = 0;
+
+    for(auto it = key_to_afters_map.cbegin(); it != key_to_afters_map.cend(); ++it) {
+
+        // cout << "Loop " << i << endl;
+        
+        // printLine(reordered_line);
+        // printMap(num_to_position);
+
+        auto curr_num = it->first;
+        auto to_swap = it->second;
+
+        if(num_to_position.find(curr_num) != num_to_position.end()) {
+
+            // cout << "Current number: " << curr_num << " position: " << num_to_position[curr_num] << endl;
+
+            for(auto swap_num = to_swap.cbegin(); swap_num != to_swap.cend(); ++swap_num) {
+
+                if(num_to_position.find(*swap_num) != num_to_position.end() && num_to_position[*swap_num] < num_to_position[curr_num]) {
+
+                    // cout << "Number to swap: " << *swap_num << " position: " << num_to_position[*swap_num] << endl;
+
+                    reordered_line.insert(reordered_line.begin() + num_to_position[*swap_num], curr_num);
+
+                    // printLine(reordered_line);
+
+                    reordered_line.erase(reordered_line.begin() + num_to_position[curr_num] + 1);
+
+                    // printLine(reordered_line);
+                    
+                    num_to_position[*swap_num] = num_to_position[*swap_num] + 1;
+                    num_to_position[curr_num] = num_to_position[*swap_num] - 1;
+                }
+            }
+
+        }
+
+        i++;
+    }
+
+    return reordered_line;
+}
+
+void partTwo(ParsedInput parsed_input) {
+
+    // printMap(parsed_input.key_to_afters_map);
+
+    int total = 0;
+
+    for(auto line: parsed_input.page_orders) {
+        if(!isLineValid(parsed_input.key_to_afters_map, line)) {
+            auto ordered_line = orderLine(parsed_input.key_to_afters_map, line);
+
+            if(!isLineValid(parsed_input.key_to_afters_map, ordered_line)) {
+                printLine(line);
+                printLine(ordered_line);
+            }
+
+            total += ordered_line[ordered_line.size() / 2];
+        }
+    }
+
+    cout << "Part two: " << total << endl;
+
+}
+
+int main() {
+
+    const auto io = IO();
+    const auto input = io.readFile("./io/day-5/real-input.txt");
+
+    ParsedInput parsed_input = parseInput(input);
+
+    // partOne(parsed_input);
+    partTwo(parsed_input);
+
+    return 0;
+}
