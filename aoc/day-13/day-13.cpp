52c10434901626ee482bb83ee4f3f925480a5690
James Collerton <jamescollerton@live.co.uk>
2024-12-14 12:06:21 +0000

    Done Day 13, adapt for part 1

--- aoc/day-13/day-13.cpp
+++ aoc/day-13/day-13.cpp
@@ -71,50 +71,80 @@ void print(Puzzle p) {
     cout << endl;
 }
 
-int bfs_find_min_tokens(Puzzle puzzle) {
+// int bfs_find_min_tokens(Puzzle puzzle) {
 
-    long min_tokens = LLONG_MAX;
-    queue<State> queue;
+//     long min_tokens = LLONG_MAX;
+//     queue<State> queue;
 
-    queue.push({0, 0, 0});
-    int level = 0;
+//     queue.push({0, 0, 0});
+//     int level = 0;
 
-    while(level < 100 && queue.size() != 0) {
-        int size = queue.size();
-        for(int i = 0; i < size; i++) {
-            State state = queue.front();
-            queue.pop();
+//     while(level < 100 && queue.size() != 0) {
+//         int size = queue.size();
+//         for(int i = 0; i < size; i++) {
+//             State state = queue.front();
+//             queue.pop();
 
-            // Only bother moving if we're going to
-            // improve on the current state
-            if(state.tokens < min_tokens) {
-                if(state.x == puzzle.p.x && state.y == puzzle.p.y) {
-                    min_tokens = min(state.tokens, min_tokens);
-                } else {
-                    // Make A move
-                    queue.push({state.x + puzzle.a.x, state.y + puzzle.a.y, state.tokens + 3});
-                    // Make B move
-                    queue.push({state.x + puzzle.b.x, state.y + puzzle.b.y, state.tokens + 1});
-                }
-            }
-        }
-        cout << "Level: " << level << endl;
-        level++;
-    }
+//             // Only bother moving if we're going to
+//             // improve on the current state
+//             if(state.tokens < min_tokens) {
+//                 if(state.x == puzzle.p.x && state.y == puzzle.p.y) {
+//                     min_tokens = min(state.tokens, min_tokens);
+//                 } else {
+//                     // Make A move
+//                     queue.push({state.x + puzzle.a.x, state.y + puzzle.a.y, state.tokens + 3});
+//                     // Make B move
+//                     queue.push({state.x + puzzle.b.x, state.y + puzzle.b.y, state.tokens + 1});
+//                 }
+//             }
+//         }
+//         cout << "Level: " << level << endl;
+//         level++;
+//     }
 
+// }
+
+int maths_find_min_tokens(Puzzle puzzle) {
+
+    int p_x = puzzle.p.x;
+    int p_y = puzzle.p.y;
+    int a_x = puzzle.a.x;
+    int a_y = puzzle.a.y;
+    int b_x = puzzle.b.x;
+    int b_y = puzzle.b.y;
+    
+    long q_numerator = ((p_y * a_x) - (p_x * a_y));
+    long q_denominator = ((a_x * b_y) - (b_x * a_y));
+
+    // cout << q_numerator << endl;
+    // cout << q_denominator << endl;
+
+    int q = q_numerator / q_denominator;
+    int r = ((p_x - q * b_x) / a_x);
+
+    // cout << r << endl;
+
+    if((r > 0 && q > 0 && r < 100 && q < 100) && (r * a_x + q * b_x == p_x) && (r * a_x + q * b_x == p_x)) {
+        cout << q << endl;
+        cout << r << endl;
+        cout << q * 3 + r * 1 << endl;
+        return r * 3 + q * 1;
+    } 
+
+    return 0;
 }
 
 void part_one() {
 
     const auto io = IO();
-    auto input = io.readFile("./io/day-13/test-input.txt");
+    auto input = io.readFile("./io/day-13/real-input.txt");
 
     long total = 0;
 
     vector<Puzzle> puzzles = parse_input(input);
     for(auto p: puzzles) {
         // print(p);
-        total += bfs_find_min_tokens(p);
+        total += maths_find_min_tokens(p);
     }
 
     cout << "Part one: " << total << endl;
